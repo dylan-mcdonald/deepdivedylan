93097061b18289ed7e36961a9d06be34bf5d2fdd
Dylan McDonald <dmcdonald21@cnm.edu>
2016-08-04 10:27:01 -0600

    Added generic webpack template

---
+++ webpack/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8" />
+		<meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<base href="/" />
+
+		<title>Deep Dive Dylan</title>
+	</head>
+	<body>
+		<main class="container">
+			<deepdivedylan-app>Loading&hellip;</deepdivedylan-app>
+		</main>
+	</body>
+</html>
