ee73a7766f8df16819d14cbab5eb05d4d3ca9dd5
Dylan McDonald <dmcdonald21@cnm.edu>
2016-08-04 11:02:30 -0600

    added the splash component

--- app/app.routes.ts
+++ app/app.routes.ts
@@ -1,5 +1,8 @@
 import {provideRouter, RouterConfig, ROUTER_DIRECTIVES} from "@angular/router";
+import {SplashComponent} from "./components/splash-component";
+
 export const routes: RouterConfig = [
+	{path: "", component: SplashComponent}
 ];
 
 export const appRouterProviders = [
