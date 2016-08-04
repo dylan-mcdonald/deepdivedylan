e2d9173ee670487420a8a9650c91fbe5432d489a
Dylan McDonald <dmcdonald21@cnm.edu>
2016-08-04 12:08:09 -0600

    moved the default route - seriously!?!? :pouting_cat:

--- app/app.routes.ts
+++ app/app.routes.ts
@@ -1,7 +1,9 @@
 import {provideRouter, RouterConfig, ROUTER_DIRECTIVES} from "@angular/router";
 import {SplashComponent} from "./components/splash-component";
+import {TeachingComponent} from "./components/teaching-component";
 
 export const routes: RouterConfig = [
+	{path: "teaching", component: TeachingComponent},
 	{path: "", component: SplashComponent}
 ];
 
