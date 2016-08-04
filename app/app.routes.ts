2adea1bee6dac1afa9e5246b9f25a3ea05591e0b
Dylan McDonald <dmcdonald21@cnm.edu>
2016-08-04 10:30:40 -0600

    added blank routes

---
+++ app/app.routes.ts
@@ -0,0 +1,7 @@
+import {provideRouter, RouterConfig, ROUTER_DIRECTIVES} from "@angular/router";
+export const routes: RouterConfig = [
+];
+
+export const appRouterProviders = [
+	provideRouter(routes)
+];
