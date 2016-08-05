0182c7776d3caa1b5f145cf77f05fa475e2d521d
Dylan McDonald <dmcdonald21@cnm.edu>
2016-08-04 20:41:46 -0600

    added font and background

--- webpack/index.php
+++ webpack/index.php
@@ -6,11 +6,42 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1" />
 		<base href="/" />
 
+		<!-- Google Fonts: Didact Gothic -->
+		<link href="//fonts.googleapis.com/css?family=Didact+Gothic" rel="stylesheet" type="text/css">
+
 		<title>Deep Dive Dylan</title>
 	</head>
-	<body>
-		<main class="container">
-			<deepdivedylan-app>Loading&hellip;</deepdivedylan-app>
-		</main>
+	<body class="sfooter">
+		<div class="sfooter-content">
+			<header id="top">
+				<nav class="navbar navbar-fixed-top">
+					<div class="container">
+						<div class="navbar-header">
+							<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapsed">
+								<span class="sr-only">Main Menu</span>
+								<i class="fa fa-bars fa-lg"></i>
+							</button>
+							<a class="navbar-brand" href="#">Deep Dive Dylan</a>
+						</div>
+						<div class="collapse navbar-collapse" id="navbar-collapsed">
+							<ul class="nav navbar-nav navbar-right">
+								<li><a href="#"><i class="fa fa-home fa-lg"></i></a></li>
+								<li><a href="#about">About</a></li>
+								<li><a href="#">Resume</a></li>
+								<li><a role="button" data-toggle="modal" data-target="#contactModal">Contact</a></li>
+							</ul>
+						</div>
+					</div>
+				</nav>
+			</header>
+			<main class="container">
+				<deepdivedylan-app>Loading&hellip;</deepdivedylan-app>
+			</main>
+		</div>
+		<footer class="footer">
+			<div class="container">
+				This is a footer
+			</div>
+		</footer>
 	</body>
 </html>
