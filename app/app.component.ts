04261bf80b95d9382ad3c72fd95e480b721c0324
Dylan McDonald <dmcdonald21@cnm.edu>
2016-08-04 21:10:48 -0600

    made the nav mobile friendly

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import {Component} from "@angular/core"
+import {NgClass} from "@angular/common";
 import {ROUTER_DIRECTIVES} from "@angular/router";
 import {HTTP_PROVIDERS} from "@angular/http";
 
 @Component({
 	selector: 'deepdivedylan-app',
 	templateUrl: './templates/deepdivedylan-app.html',
-	directives: [ROUTER_DIRECTIVES],
+	directives: [NgClass, ROUTER_DIRECTIVES],
 	providers: [HTTP_PROVIDERS]
 })
 
-export class AppComponent {}
+export class AppComponent {
+	navCollapse = true;
+
+	toggleCollapse() {
+		this.navCollapse = !this.navCollapse;
+	}
+}
