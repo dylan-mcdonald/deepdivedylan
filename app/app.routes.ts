2d0c6778a54e187152741f98e19ff941f3d80180
Dylan McDonald <dmcdonald21@cnm.edu>
2016-08-04 20:10:00 -0600

    added coding component

--- app/app.routes.ts
+++ app/app.routes.ts
@@ -1,8 +1,10 @@
 import {provideRouter, RouterConfig, ROUTER_DIRECTIVES} from "@angular/router";
+import {CodingComponent} from "./components/coding-component";
 import {SplashComponent} from "./components/splash-component";
 import {TeachingComponent} from "./components/teaching-component";
 
 export const routes: RouterConfig = [
+	{path: "coding", component: CodingComponent},
 	{path: "teaching", component: TeachingComponent},
 	{path: "", component: SplashComponent}
 ];
