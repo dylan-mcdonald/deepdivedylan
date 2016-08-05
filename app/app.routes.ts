6e903e68c91cb7f8d29c1ec6b69f2f599298e5c8
Dylan McDonald <dmcdonald21@cnm.edu>
2016-08-04 21:00:12 -0600

    added contact placeholder

--- app/app.routes.ts
+++ app/app.routes.ts
@@ -1,5 +1,6 @@
 import {provideRouter, RouterConfig, ROUTER_DIRECTIVES} from "@angular/router";
 import {AdvocacyComponent} from "./components/advocacy-component";
+import {ContactComponent} from "./components/contact-component";
 import {CodingComponent} from "./components/coding-component";
 import {SplashComponent} from "./components/splash-component";
 import {TeachingComponent} from "./components/teaching-component";
@@ -7,6 +8,7 @@ import {TeachingComponent} from "./components/teaching-component";
 export const routes: RouterConfig = [
 	{path: "advocacy", component: AdvocacyComponent},
 	{path: "coding", component: CodingComponent},
+	{path: "contact", component: ContactComponent},
 	{path: "teaching", component: TeachingComponent},
 	{path: "", component: SplashComponent}
 ];
---
+++ app/components/contact-component.ts
@@ -0,0 +1,7 @@
+import {Component} from "@angular/core";
+
+@Component({
+	templateUrl: "./templates/contact.html"
+})
+
+export class ContactComponent {}
