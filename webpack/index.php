693cc1d8ddeabac02d69182e0a9f22533f590613
Dylan McDonald <dmcdonald21@cnm.edu>
2016-08-04 20:53:25 -0600

    routed the nav via Angular 2

--- webpack/index.php
+++ webpack/index.php
@@ -12,36 +12,6 @@
 		<title>Deep Dive Dylan</title>
 	</head>
 	<body class="sfooter">
-		<div class="sfooter-content">
-			<header id="top">
-				<nav class="navbar navbar-fixed-top">
-					<div class="container">
-						<div class="navbar-header">
-							<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapsed">
-								<span class="sr-only">Main Menu</span>
-								<i class="fa fa-bars fa-lg"></i>
-							</button>
-							<a class="navbar-brand" href="#">Deep Dive Dylan</a>
-						</div>
-						<div class="collapse navbar-collapse" id="navbar-collapsed">
-							<ul class="nav navbar-nav navbar-right">
-								<li><a href="#"><i class="fa fa-home fa-lg"></i></a></li>
-								<li><a href="#about">About</a></li>
-								<li><a href="#">Resume</a></li>
-								<li><a role="button" data-toggle="modal" data-target="#contactModal">Contact</a></li>
-							</ul>
-						</div>
-					</div>
-				</nav>
-			</header>
-			<main class="container">
-				<deepdivedylan-app>Loading&hellip;</deepdivedylan-app>
-			</main>
-		</div>
-		<footer class="footer">
-			<div class="container">
-				This is a footer
-			</div>
-		</footer>
+		<deepdivedylan-app>Loading&hellip;</deepdivedylan-app>
 	</body>
 </html>
