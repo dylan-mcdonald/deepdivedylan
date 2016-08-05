49495069025cc480393acc08184e4f6837bd5a5d
Dylan McDonald <dmcdonald21@cnm.edu>
2016-08-04 20:15:04 -0600

    added advocacy component

--- app/app.routes.ts
+++ app/app.routes.ts
@@ -1,9 +1,11 @@
 import {provideRouter, RouterConfig, ROUTER_DIRECTIVES} from "@angular/router";
+import {AdvocacyComponent} from "./components/advocacy-component";
 import {CodingComponent} from "./components/coding-component";
 import {SplashComponent} from "./components/splash-component";
 import {TeachingComponent} from "./components/teaching-component";
 
 export const routes: RouterConfig = [
+	{path: "advocacy", component: AdvocacyComponent},
 	{path: "coding", component: CodingComponent},
 	{path: "teaching", component: TeachingComponent},
 	{path: "", component: SplashComponent}
