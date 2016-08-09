262dc5da5fe0c7e25c21f7e2a80db0fa1ec3707e
Dylan McDonald <dmcdonald21@cnm.edu>
2016-08-08 23:46:12 -0600

    added sendMail() method to contact component

--- app/components/contact-component.ts
+++ app/components/contact-component.ts
@@ -1,7 +1,22 @@
 import {Component} from "@angular/core";
+import {MailMessage} from "../classes/mail-message";
+import {MailResponse} from "../classes/mail-response";
+import {MailService} from "../services/mail-service";
 
 @Component({
+	providers: [MailService],
 	templateUrl: "./templates/contact.html"
 })
 
-export class ContactComponent {}
+export class ContactComponent {
+	constructor(private mailService: MailService) {}
+
+	mailResponse: MailResponse = null;
+
+	sendMail(mailMessage: MailMessage) {
+		this.mailService.sendMail(mailMessage)
+			.subscribe(
+				mailResponse => this.mailResponse = mailResponse
+			);
+	}
+}
