0975cb7481fede22d4e18c6f843085535178a243
Dylan McDonald <dmcdonald21@cnm.edu>
2016-08-15 21:39:22 -0600

    added resume class

---
+++ app/classes/resume.ts
@@ -0,0 +1,13 @@
+export class Resume {
+	constructor(
+		public settings: any,
+		public firstname: string,
+		public familyname: string,
+		public linkedin_id: string,
+		public github_id: string,
+		public bio_data: any,
+		public skills: any,
+		public education: any,
+		public experience: any,
+		public research_experience: any) {}
+}
