7137dd25d21735e8a164625a2606be99f97c64dd
Dylan McDonald <dmcdonald21@cnm.edu>
2016-09-10 11:56:43 -0600

    deleted commented out code hehe

--- app/app.ts
+++ app/app.ts
@@ -1,7 +1,5 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app.module';
 
-// browserDynamicPlatform().bootstrapModule(AppModule);
-
 const platform = platformBrowserDynamic();
 platform.bootstrapModule(AppModule);
