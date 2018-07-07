b4c5ba77056a78e2d7e6dd200a38456033630e3d
Dylan McDonald <dmcdonald21@cnm.edu>
2018-07-07 12:55:26 -0600

    refactored HTTP interceptor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,8 @@
 import {NgModule} from "@angular/core";
 import {BrowserModule} from "@angular/platform-browser";
 import {FormsModule} from "@angular/forms";
-import {HttpModule} from "@angular/http";
-import {ReCaptchaModule} from 'angular2-recaptcha/angular2-recaptcha';
+import {HttpClientModule} from "@angular/common/http";
+import {ReCaptchaModule} from "angular2-recaptcha";
 import {AppComponent} from "./app.component";
 import {allAppComponents, appRoutingProviders, routing} from "./app.routes";
 import {MailService} from "./services/mail-service";
@@ -11,7 +11,7 @@ import {ResumeService} from "./services/resume-service";
 const moduleDeclarations = [AppComponent];
 
 @NgModule({
-	imports:      [BrowserModule, FormsModule, HttpModule, ReCaptchaModule, routing],
+	imports:      [BrowserModule, FormsModule, HttpClientModule, ReCaptchaModule, routing],
 	declarations: [...moduleDeclarations, ...allAppComponents],
 	bootstrap:    [AppComponent],
 	providers:    [appRoutingProviders, MailService, ResumeService]
