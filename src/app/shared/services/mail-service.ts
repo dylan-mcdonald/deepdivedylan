558878aa422cd6289fc4dc168a1a68eae9b6ac07
Dylan McDonald <dmcdonald21@cnm.edu>
2018-07-07 13:16:26 -0600

    refactored mail service

--- src/app/shared/services/mail-service.ts
+++ src/app/shared/services/mail-service.ts
@@ -1,25 +1,16 @@
 import {Injectable} from "@angular/core";
-import {Headers, Http, Response, RequestOptions} from "@angular/http";
-import {Observable} from "rxjs/Observable";
-import {BaseService} from "./base-service";
-import {MailMessage} from "../classes/mail-message";
-import {MailResponse} from "../classes/mail-response";
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {MailMessage} from "../classes/mail.message";
+import {MailResponse} from "../classes/mail.response";
 
 @Injectable()
-export class MailService extends BaseService {
-	constructor(protected http: Http) {
-		super(http);
-	}
+export class MailService {
+	constructor(protected http: HttpClient) {}
 
-	private mailUrl = "/mail/";
+	private mailUrl = "/api/mail/";
 
 	sendMail(mailMessage: MailMessage): Observable<MailResponse> {
-		let body = JSON.stringify(mailMessage);
-		let headers = new Headers({ "Content-Type": "application/json" });
-		let options = new RequestOptions({ headers: headers });
-
-		return(this.http.post(this.mailUrl, body, options)
-			.map(this.extractData)
-			.catch(this.handleError));
+		return(this.http.post<MailResponse>(this.mailUrl, mailMessage));
 	}
 }
