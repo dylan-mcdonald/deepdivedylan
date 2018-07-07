e957925480d8a58325f2423da816645e6c2bff55
Dylan McDonald <dmcdonald21@cnm.edu>
2018-07-07 14:14:36 -0600

    waited on the ready event to load components

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,10 @@ import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
 import {AppModule} from './app/app.module';
 import {enableProdMode} from '@angular/core';
 
-if(process.env.ENV === "live") {
-	enableProdMode();
-}
-const platform = platformBrowserDynamic();
-platform.bootstrapModule(AppModule);
+window.addEventListener("DOMContentLoaded", event => {
+	if(process.env.ENV === "live") {
+		enableProdMode();
+	}
+	const platform = platformBrowserDynamic();
+	platform.bootstrapModule(AppModule);
+});
