18f902963de5e3ccc3d59b6fc758f69fcf1748c4
Dylan McDonald <dmcdonald21@cnm.edu>
2018-07-07 12:02:44 -0600

    upgraded vendor

--- src/vendor.ts
+++ src/vendor.ts
@@ -1,16 +1,19 @@
-// Angular 2
-import '@angular/platform-browser';
-import '@angular/platform-browser-dynamic';
-import '@angular/core';
-import '@angular/common';
-import '@angular/http';
-import '@angular/router';
+// Angular 5
+import "@angular/platform-browser";
+import "@angular/platform-browser-dynamic";
+import "@angular/core";
+import "@angular/common";
+import "@angular/router";
 
 // RxJS
-import 'rxjs';
+import "rxjs";
 
-// Other vendors for example jQuery, Lodash or Bootstrap
+// Other vendors for example Lodash or Bootstrap
 // You can import js, ts, css, sass, ...
-import 'angular2-recaptcha';
-import 'bootstrap-loader';
-import 'font-awesome-webpack2';
+
+
+// font awesome
+import "@fortawesome/fontawesome";
+import "@fortawesome/fontawesome-free-brands";
+import "@fortawesome/fontawesome-free-regular";
+import "@fortawesome/fontawesome-free-solid";
