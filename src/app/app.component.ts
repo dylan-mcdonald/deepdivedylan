4c44caa57d1f7e98750cfbb47a1e792b6e473a4a
Dylan McDonald <dmcdonald21@cnm.edu>
2018-07-08 22:16:06 -0600

    used session status on app load

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,26 @@
-import {Component} from "@angular/core";
+import {Component, OnInit} from "@angular/core";
 import {faBars, faHome} from "@fortawesome/free-solid-svg-icons";
+import {MailResponse} from "./shared/classes/mail.response";
+import {SessionService} from "./shared/services/session.service";
 
 @Component({
 	selector: "deepdivedylan-app",
 	template: require("./app.component.html")
 })
 
-export class AppComponent {
+export class AppComponent implements OnInit {
 	faBars = faBars;
 	faHome = faHome;
 	navCollapse = true;
+
+	response: MailResponse = null;
+
+	constructor(protected sessionService: SessionService) {}
+
+	ngOnInit(): void {
+		this.sessionService.setSession()
+			.subscribe(response => this.response = response);
+	}
 
 	toggleCollapse() {
 		this.navCollapse = !this.navCollapse;
