5308610e3d5099e199212b26c49d58a820c33196
Dylan McDonald <dmcdonald21@cnm.edu>
2018-07-08 20:23:40 -0600

    registered FontAwesome service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import {BrowserModule} from "@angular/platform-browser";
 import {FormsModule} from "@angular/forms";
 import {HttpClientModule} from "@angular/common/http";
 import {ReCaptchaModule} from "angular2-recaptcha";
+import {FontAwesomeModule} from "@fortawesome/angular-fontawesome";
 import {AppComponent} from "./app.component";
 import {allAppComponents, appRoutingProviders, routing} from "./app.routes";
 import {MailService} from "./shared/services/mail.service";
@@ -11,7 +12,7 @@ import {ResumeService} from "./shared/services/resume.service";
 const moduleDeclarations = [AppComponent];
 
 @NgModule({
-	imports:      [BrowserModule, FormsModule, HttpClientModule, ReCaptchaModule, routing],
+	imports:      [BrowserModule, FormsModule, HttpClientModule, FontAwesomeModule, ReCaptchaModule, routing],
 	declarations: [...moduleDeclarations, ...allAppComponents],
 	bootstrap:    [AppComponent],
 	providers:    [appRoutingProviders, MailService, ResumeService]
