20444cfb816037a2660829ba25bb6ec32784a40c
Dylan McDonald <dmcdonald21@cnm.edu>
2018-07-08 20:53:25 -0600

    converted icons on resume

--- src/app/resume/resume.component.ts
+++ src/app/resume/resume.component.ts
@@ -1,12 +1,26 @@
 import {Component, OnInit} from "@angular/core";
 import {Resume} from "../shared/classes/resume";
 import {ResumeService} from "../shared/services/resume.service";
+import {faBook, faBriefcase, faCode, faEnvelope, faGlobe, faGraduationCap, faPhone, faUser} from "@fortawesome/free-solid-svg-icons";
+import {faGithub, faLinkedin, faTwitter} from "@fortawesome/free-brands-svg-icons";
 
 @Component({
 	template: require("./resume.component.html")
 })
 
 export class ResumeComponent implements OnInit {
+	faBook = faBook;
+	faBriefcase = faBriefcase;
+	faCode = faCode;
+	faEnvelope = faEnvelope;
+	faGithub = faGithub;
+	faGlobe = faGlobe;
+	faGraduationCap = faGraduationCap;
+	faLinkedin = faLinkedin;
+	faPhone = faPhone;
+	faTwitter = faTwitter;
+	faUser = faUser;
+
 	constructor(private resumeService: ResumeService) {}
 
 	resumeData: Resume = null;
