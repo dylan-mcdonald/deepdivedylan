7c086e0e581e75dacb6851f9de360430302607e3
Dylan McDonald <dmcdonald21@cnm.edu>
2018-08-20 19:46:30 -0600

    look at all the pretty stars :star:

--- src/app/resume/resume.component.ts
+++ src/app/resume/resume.component.ts
@@ -1,7 +1,7 @@
 import {Component, OnInit} from "@angular/core";
 import {Resume} from "../shared/classes/resume";
 import {ResumeService} from "../shared/services/resume.service";
-import {faBook, faBriefcase, faCode, faCodeBranch, faEnvelope, faGlobe, faGraduationCap, faPhone, faUser} from "@fortawesome/free-solid-svg-icons";
+import {faBook, faBriefcase, faCode, faCodeBranch, faEnvelope, faGlobe, faGraduationCap, faPhone, faStar, faUser} from "@fortawesome/free-solid-svg-icons";
 import {faGithub, faLinkedin, faTwitter} from "@fortawesome/free-brands-svg-icons";
 
 @Component({
@@ -20,6 +20,7 @@ export class ResumeComponent implements OnInit {
 	faGraduationCap = faGraduationCap;
 	faLinkedin = faLinkedin;
 	faPhone = faPhone;
+	faStar = faStar;
 	faTwitter = faTwitter;
 	faUser = faUser;
 
